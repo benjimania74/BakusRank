5dd5f9d2a4939e0e9bfa5f44658f14a5f6ea7b78
benjimania74 <lesdalton74@gmail.com>
2021-07-11 21:32:03 +0200

    Version 1.1.0 pour MC 1.12.X -> 1.16.X

--- upload.bat
+++ upload.bat
@@ -1,6 +1,6 @@
 git init
 git add *
-git commit -m "Version 1.1.0 pour MC 1.16.X"
+git commit -m "Version 1.1.0 pour MC 1.12.X -> 1.16.X"
 git branch -M main
 git remote add origin https://github.com/benjimania74/BakusRank.git
 git push -u origin main
